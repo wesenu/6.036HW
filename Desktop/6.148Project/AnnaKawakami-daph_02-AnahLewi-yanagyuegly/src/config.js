3b13bf128bafec75ed035eae6072107cbf78f762
Yue Yang <yueyang@YuedeMacBook-Air.local>
2019-01-13 13:51:31 -0500

    api error

---
+++ Desktop/6.148Project/AnnaKawakami-daph_02-AnahLewi-yanagyuegly/src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'iCAwDM1eX3NpprOjGbssUd0Eg',
+    consumer_secret: '59xPw0xcozGmViW9Uns0BD13qX4dI3UvTGyUsGesm37wHrVYOm',
+    access_token_key: '1048362184781312006-WxlUIPZi2KHIIcQh8fb9MWT13PouCo',
+    access_token_secret: '2cvWVzQii5aDn0plJndUIpbCrnBVCtOQKLP4EoYMlSbBT'
+  }
